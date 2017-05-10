8635a81ac95681ce8f925fd3cde8301064791d95
Jun Zhao <zhaojun7987@gmail.com>
2017-05-10 15:40:22 -0400

    update proc script to support pre-trained embs

--- proc.py
+++ proc.py
@@ -13,12 +13,15 @@ tf.app.flags.DEFINE_integer('max_vocab_size', 1000, 'Max vocabulary size.')
 tf.app.flags.DEFINE_float('test_split', .2, 'Split for testing data.')
 tf.app.flags.DEFINE_float('valid_split', .2, 'Split for validation data.')
 tf.app.flags.DEFINE_bool('split_data', False, 'Split dataset.')
+tf.app.flags.DEFINE_bool('pretrained_embs', False, 'Use word vectors.')
 
 FLAGS = tf.app.flags.FLAGS
 
 
 def init(inputs):
-    tokens = {}
+    if not FLAGS.pretrained_embs:
+        tokens = {}
+
     labels = set()
     lengths = []
 
@@ -35,23 +38,49 @@ def init(inputs):
                 else:
                     length += 1
                     token, label = line.split()
-                    if token in tokens:
-                        tokens[token] += 1
-                    else:
-                        tokens[token] = 1
+                    if not FLAGS.pretrained_embs:
+                        if token in tokens:
+                            tokens[token] += 1
+                        else:
+                            tokens[token] = 1
                     labels.add(label)
 
-    vocab = ['<pad>', '<unk>'] + sorted(tokens, key=tokens.get, reverse=True)
-
-    if len(vocab) > FLAGS.max_vocab_size:
-        vocab = vocab[:FLAGS.max_vocab_size]
-
     labels = ['P'] + sorted(labels)
-    return vocab, labels, max(lengths)
+
+    if FLAGS.pretrained_embs:
+        vocab, emb_size = get_vocab()
+        return vocab, labels, max(lengths), emb_size
+    else:
+        vocab = get_vocab(tokens)
+        return vocab, labels, max(lengths)
 
 
 def get_path(fn):
     return os.path.join(FLAGS.data_dir, fn)
+
+
+def get_vocab(tokens=None):
+    specials = ['<pad>', '<unk>']
+    if tokens:
+        vocab = specials + sorted(tokens, key=tokens.get, reverse=True)
+        if len(vocab) > FLAGS.max_vocab_size:
+            vocab = vocab[:FLAGS.max_vocab_size]
+        return vocab
+    else:
+        embs = glob.glob(get_path('*.vec'))
+        tokens = []
+
+        if len(embs) == 0:
+            raise Exception('Did not find file with .vec ext.')
+        elif len(embs) > 1:
+            raise Exception('Found more than one file with .vec ext.')
+
+        with open(embs[0]) as f:
+            for i, line in enumerate(f):
+                if i == 0:
+                    emb_size = len(line.split()) - 1
+                tokens.append(line.split()[0])
+        return specials + tokens, emb_size
 
 
 def split_data(inputs):
@@ -112,8 +141,14 @@ def proc():
         for k in ['train', 'valid', 'test']:
             files[k] = glob.glob(get_path('*.%s.iob' % k))
 
-    _vocab, _labels, _length = init(
-        files['train'] + files['valid'] + files['test'])
+    if FLAGS.pretrained_embs:
+        _vocab, _labels, _length, emb_size = init(
+            files['train'] + files['valid'] + files['test'])
+        print('Embedding size: %i' % emb_size)
+
+    else:
+        _vocab, _labels, _length = init(
+            files['train'] + files['valid'] + files['test'])
 
     print('Label size: %i' % len(_labels))
     print('Vocab size: %i' % len(_vocab))
@@ -152,7 +187,9 @@ def proc():
 
                         labels.append(_labels.index(label))
 
-    write_list(_vocab, 'vocab')
+    if FLAGS.pretrained_embs:
+        write_list(_vocab, 'vocab')
+
     write_list(_labels, 'labels')
 
     for k, v in data.iteritems():
